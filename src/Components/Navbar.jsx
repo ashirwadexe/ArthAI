a91528b69ffdc771f867d0f494f56f7b1d9d1b65
ashirwadexe <ashirwadbappy@gmail.com>
2026-02-21 20:37:03 +0530

    navbar push

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  
+  const navigate = useNavigate();
+
+  const logoutUser = () => {
+      navigate('/');
+  };
+
   return (
-    <div>Navbar</div>
+    <div className='shadow bg-white'>
+        <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all'>
+            <Link to='/'>
+                <img src="/logo.png" alt="logo"  className='h-5 w-auto'/>
+            </Link>
+
+            <div className='flex items-center gap-4 text-sm'>
+                <p className='max-sm:hidden'>Hi, Ashirwad</p>
+                <button 
+                    onClick={logoutUser}
+                    className='bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all cursor-pointer'
+                >
+                        Logout
+                </button>
+            </div>
+        </nav>
+    </div>
   )
 }
 
